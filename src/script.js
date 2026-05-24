38a4eaa71205be74118c0b71527947ea52161330
Nur Syamsi <syamsinur455@gmail.com>
2026-05-24 20:32:23 +0700

    add component  search bar

--- src/script.js
+++ src/script.js
@@ -1,13 +1,27 @@
+// navigasi
 const itemNav = document.querySelector('#item-nav');
 const hamburgerBtn = document.querySelector('#hamburger');
+const searchButton = document.querySelector('#search-btn');
+const searchForm = document.querySelector('#search-form');
+const searchBox = document.querySelector('#search-box');
 
-hamburgerBtn.addEventListener('click', function() {
+hamburgerBtn.addEventListener('click', function(e) {
     itemNav.classList.toggle('active');
-    hamburgerBtn.preventDefault();
+    e.preventDefault();
+});
+
+searchButton.addEventListener('click', function(e) {
+    searchForm.classList.toggle('search-active');
+    searchBox.focus();
+    e.preventDefault();
 });
 
 document.addEventListener('click', function(e) {
     if(!hamburgerBtn.contains(e.target) && !itemNav.contains(e.target)) {
         itemNav.classList.remove('active');
+    }
+
+    if(!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('search-active');
     }
 });
