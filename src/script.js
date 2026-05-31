c260feaa57cdbe784ea9dc2afe6745a8fd9aa532
Nur Syamsi <syamsinur455@gmail.com>
2026-05-31 20:53:57 +0700

    add cart page

--- src/script.js
+++ src/script.js
@@ -1,18 +1,29 @@
 // navigasi
 const itemNav = document.querySelector('#item-nav');
 const hamburgerBtn = document.querySelector('#hamburger');
-const searchButton = document.querySelector('#search-btn');
-const searchForm = document.querySelector('#search-form');
-const searchBox = document.querySelector('#search-box');
 
 hamburgerBtn.addEventListener('click', function(e) {
     itemNav.classList.toggle('active');
     e.preventDefault();
 });
 
+// search
+const searchButton = document.querySelector('#search-btn');
+const searchForm = document.querySelector('#search-form');
+const searchBox = document.querySelector('#search-box');
+
 searchButton.addEventListener('click', function(e) {
     searchForm.classList.toggle('search-active');
     searchBox.focus();
+    e.preventDefault();
+});
+
+// shopping cart
+const shoppingBtn = document.querySelector('#shopping-btn');
+const shoppingCart = document.querySelector('#shopping-cart');
+
+shoppingBtn.addEventListener('click', function(e) {
+    shoppingCart.classList.toggle('active');
     e.preventDefault();
 });
 
@@ -24,4 +35,10 @@ document.addEventListener('click', function(e) {
     if(!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
         searchForm.classList.remove('search-active');
     }
+
+    if(!shoppingBtn.contains(e.target) && !shoppingCart.contains(e.target)) {
+        shoppingCart.classList.remove('active');
+    }
 });
+
+
